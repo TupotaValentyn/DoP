54dbdb1587106fffe7ec83b83a5febb06f0fc5cd
WarriorKozak <gm.tupota.valik@gmail.com>
2018-09-29 14:57:09 +0300

    Merge branch 'master' of https://github.com/WarriorKozak/DoP
    # Conflicts:
    #	.idea/workspace.xml
    #	api/server.js

--- src/components/pages/old/Old.js
+++ src/components/pages/old/Old.js
@@ -2,10 +2,25 @@ import React from 'react'
 import { apiTest } from '../../../api'
 
 export default class Old extends React.Component{
+
+  constructor(props) {
+    super(props);
+    apiTest((err, timestamp) => this.setState({
+      timestamp
+    }));
+  }
+
+  state = {
+    timestamp: 'no timestamp yet'
+  };
+
+
+
   render(){
     return(
       <div className='old_question'>
         <button onClick={apiTest}>for lex</button>
+        This is the timer value: {this.state.timestamp}
       </div>
     )
   }
