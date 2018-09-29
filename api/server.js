3f0f313f5e3909344a44a88f1eb3254ceecff757
aleseyko <aleseyko@gmail.com>
2018-09-29 11:42:02 +0300

    Add models and start server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,15 @@
-const Question = require("./DoP/api/models/question");
+const Question = require("./models/Question");
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:1")
+mongoose.connect("mongodb://localhost/dop",{ useNewUrlParser: true });
+
+let oneQuestion = new Question({
+    author: "Oleksiy",
+    question: "Some question this"
+});
+
+oneQuestion.save((err)=>{
+    if (err) throw err;
+    console.log("Successfully saved")
+});
