ddc1b70f3f87ad4e1a02c76590ad9999c2424b9f
WarriorKozak <gm.tupota.valik@gmail.com>
2018-09-29 23:22:01 +0300

    Merge branch 'master' of https://github.com/WarriorKozak
    Young

--- src/components/pages/young/Question/Question.js
+++ src/components/pages/young/Question/Question.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { apiTest, getQuestion } from '../../../../api'
+import Li from '../../../molecules/Li/Li'
 
 
 export default class QuestionY extends React.Component{
@@ -10,7 +11,7 @@ export default class QuestionY extends React.Component{
       timestamp
     }));
     this.state = {
-      timestamp: 'no timestamp yet',
+      timestamp: undefined,
       value: ''
     };
 
@@ -22,14 +23,40 @@ export default class QuestionY extends React.Component{
   }
 
 
-
   render(){
-    console.log(this.state.timestamp)
+    const { timestamp } = this.state;
+    console.log(timestamp)
+    if(timestamp === undefined) {
+      return(
+        <div className='question_young'>
+          <h3>hello from question young</h3>
+        </div>
+      )
+    }
+
+    console.log(`this - ${timestamp.data['0'].author}`)
+
+    let items = timestamp.data.map( item => {
+       return (<li>Author:{item.author} Created: {item.question}</li>)
+    });
+
+    let items1 = timestamp.data.map( item => {
+      return (<Li name = {`Author:${item.question}`} />)
+    });
+
 
     return(
       <div className='question_young'>
-        <h3>hello from question young</h3>
+        <h3>data received</h3>
+        <ul>
+          {items}
+          <Li name={timestamp.data['0'].author}/>
+          <hr/>
+          {items1}
+        </ul>
       </div>
     )
+
+
   }
 }
